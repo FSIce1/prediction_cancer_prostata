3d15e851bd7e6cfc4f18c27787c405eedded673a
LuisFelipe <luisfelipesiesquen22@gmail.com>
2023-10-04 22:04:37 -0500

    conexión msql

--- cancer_prostata/settings.py
+++ cancer_prostata/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cancer_prostata.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'prediction_cancer_prostata',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
